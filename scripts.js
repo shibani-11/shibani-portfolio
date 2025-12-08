c8454232b957b7adf5f370043ecc7060da4ec3db
Shibani Kumar <shibanik11@gmail.com>
2025-12-08 17:56:57 -0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling for navbar links
-document.querySelectorAll('.navbar a').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href'))
-                .scrollIntoView({ behavior: 'smooth' });
-    });
-});
